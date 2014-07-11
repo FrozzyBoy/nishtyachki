a2fbf31a3a79a306adf07185c3b37296ccf95136
Dzianis Pasiukou <dzianis_pasiukou@epam.com>
2014-07-11 17:39:39 +0300

    add identity

---
+++ nishtyachki/Identity_test2/Identity_test2/App_Code/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminApp.App_Code
+{
+    public enum Role
+    {
+        Premium,
+        Standart
+    }
+    public class User
+    {
+        
+        public string UserName { get; set; }
+        public Statistics Stat { get; set; }
+        public Role UserRole { get; set; }       
+    }
+}
