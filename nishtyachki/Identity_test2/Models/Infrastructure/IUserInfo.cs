a2bd553243fb52b665cae089dc6b7554bc71d6be
Jenka <Yauheni_Lavitsky@epam.com>
2014-07-16 15:21:01 +0300

    ...

---
+++ nishtyachki/Identity_test2/Models/Infrastructure/IUserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminApp.Models.Infrastructure
+{
+    public interface IUserInfo
+    {
+        void UsersUseTimeCount(out DateTime[] time, out int[] count);
+    }
+}
