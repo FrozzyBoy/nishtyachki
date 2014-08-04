0ebe567fb745a5d51e159d9195f40d0a0eb8afb4
Artsiom Marozau <Artsiom_Marozau@epam.com>
2014-08-04 15:15:14 +0300

    drag and drop queue
    Signed-off-by: Artsiom Marozau <Artsiom_Marozau@epam.com>

--- nishtyachki/Identity_test2/Scripts/AppScripts/App.js
+++ nishtyachki/Identity_test2/Scripts/AppScripts/App.js
@@ -1,2 +1,2 @@
-﻿var myApp = angular.module("myApp", []);
+﻿var myApp = angular.module("myApp", ['ngDragDrop']);
 myApp.value('$', $);
---
+++ nishtyachki/Identity_test2/Scripts/AppScripts/dndControl.js
@@ -0,0 +1,6 @@
+﻿myApp.controller('dndCntrl', function ($scope, $timeout) {
+    $scope.list = ['one', 'two', 'three'];
+    $scope.change = function () {
+        console.log('darag or drop, i don`t know');
+    }
+});
