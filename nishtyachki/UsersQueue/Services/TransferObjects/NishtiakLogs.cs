b6bcb5a9cbd29bffa006f370dc96408eed339f7b
Artsiom Marozau <artsiom_marozau@epam.com>
2014-12-03 12:28:34 +0300

    stable version

---
+++ nishtyachki/UsersQueue/Services/TransferObjects/NishtiakLogs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace UsersQueue.Services.TransferObjects
+{
+    [Table("nishtiak_logs")]
+    public class NishtiakLogs
+    {
+        [Key]
+        public int ID { get; set; }
+        [DataMember]
+        public string NishtiakId { get; set; }
+        [DataMember]
+        public string ChangeWas { get; set; }
+        [DataMember]
+        public DateTime? ChangeHappend { get; set; }
+        [DataMember]
+        public int State { get; set; }
+        [DataMember]        
+        public virtual QueueUserTransferObject Owner { get; set; }
+
+        public NishtiakLogs()
+        {
+            ChangeHappend = DateTime.Now;
+        }
+    }
+}
---
+++ nishtyachki/UsersQueue/Services/TransferObjects/UserInfoLogs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace UsersQueue.Services.TransferObjects
+{
+    [Table("userinfo_logs")]
+    public class UserInfoLogs
+    {
+        [Key]
+        public int ID { get; set; }
+        [DataMember]
+        public int State { get; set; }
+        [DataMember]
+        public int Role { get; set; }
+        [DataMember]
+        public DateTime PremiumEndDate { get; set; }
+
+    }
+}
